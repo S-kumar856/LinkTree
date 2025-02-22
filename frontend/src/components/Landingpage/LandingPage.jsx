ad4d3a1fd810799cca0d3dc8fa21d1dfdc1ac5e1
S KUMAR <hk856856@gmail.com>
2025-02-23 00:22:56 +0530

    completed landing page

--- frontend/src/components/Landingpage/LandingPage.jsx
+++ frontend/src/components/Landingpage/LandingPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import style from './LandingPage.module.css'
+import { FaTwitter, FaInstagram, FaYoutube, FaTiktok, FaFire } from 'react-icons/fa';
 import sparklogo from '../../assets/spark.png'
 import analytic1img from '../../assets/Analytics1.png'
 import analytic2img from '../../assets/div.png'
 import analytic3img from '../../assets/div1.png'
+import flowericon from '../../assets/icon.png'
+import frame1 from '../../assets/frame/Frame1.png'
+import frame2 from '../../assets/frame/Frame2.png'
+import frame3 from '../../assets/frame/Frame3.png'
+import frame4 from '../../assets/frame/Frame4.png'
+import frame5 from '../../assets/frame/Frame5.png'
+import frame6 from '../../assets/frame/Frame6.png'
+import frame7 from '../../assets/frame/Frame7.png'
+import frame8 from '../../assets/frame/Frame8.png'
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+    const navigate = useNavigate();
     return (
         <>
             <div className={style.main_container}>
@@ -15,7 +27,7 @@ const LandingPage = () => {
                         <span className={style.sparkTitle}>SPARK</span> | <span className={style.market}>Marketplace</span>
                     </div>
                     <div className={style.nav_btn}>
-                        <button>Sign up free</button>
+                        <button onClick={()=>navigate('/register')}>Sign up free</button>
                     </div>
                 </nav>
                 {/* container2 */}
@@ -75,7 +87,7 @@ const LandingPage = () => {
                         <button className={style.customer_btn}>Read customer stories</button>
                     </div>
                     <div className={style.container5_right}>
-                        <span className={style.icon}>✳️</span>
+                        <span className={style.icon}><img src={flowericon} alt="flowericon" /></span>
                         <p>
                             <strong>[short description goes in here]</strong> Lorem ipsum is a placeholder text to demonstrate.
                         </p>
@@ -83,6 +95,206 @@ const LandingPage = () => {
                 </div>
 
                 {/* container5 end */}
+
+                {/* container6 start */}
+                <div className={style.container6}>
+
+                    <div className={style.testimonial_cardGray}>
+                        <h3>Amazing tool! Saved me months</h3>
+                        <p>
+                            This is a placeholder for your testimonials and what your client has to say,
+                            put them here and make sure it’s 100% true and meaningful.
+                        </p>
+                        <div className={style.author_section}>
+                            <div className={style.avatar}></div>
+                            <div>
+                                <strong>John Master</strong>
+                                <p>Director, Spark.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.testimonial_card}>
+                        <h3>Amazing tool! Saved me months</h3>
+                        <p>
+                            This is a placeholder for your testimonials and what your client has to say,
+                            put them here and make sure it’s 100% true and meaningful.
+                        </p>
+                        <div className={style.author_section}>
+                            <div className={style.avatar}></div>
+                            <div>
+                                <strong>John Master</strong>
+                                <p>Director, Spark.com</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={style.testimonial_card}>
+                        <h3>Amazing tool! Saved me months</h3>
+                        <p>
+                            This is a placeholder for your testimonials and what your client has to say,
+                            put them here and make sure it’s 100% true and meaningful.
+                        </p>
+                        <div className={style.author_section}>
+                            <div className={style.avatar}></div>
+                            <div>
+                                <strong>John Master</strong>
+                                <p>Director, Spark.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.testimonial_cardGray}>
+                        <h3>Amazing tool! Saved me months</h3>
+                        <p>
+                            This is a placeholder for your testimonials and what your client has to say,
+                            put them here and make sure it’s 100% true and meaningful.
+                        </p>
+                        <div className={style.author_section}>
+                            <div className={style.avatar}></div>
+                            <div>
+                                <strong>John Master</strong>
+                                <p>Director, Spark.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* container6 end */}
+
+                {/* container7 start */}
+                <div className={style.container7}>
+                    <h2>All Link Apps and Integrations</h2>
+                    <div className={style.appGrids}>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame1} alt="" /></div>
+                            <div>
+                                <strong>Audiomack</strong>
+                                <p>Add an Audiomack player to your Linktree</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame2} alt="" /></div>
+                            <div>
+                                <strong>Bandsintown</strong>
+                                <p>Drive ticket sales by listing your events</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame3} alt="" /></div>
+                            <div>
+                                <strong>Bonfire</strong>
+                                <p>Display and sell your custom merch</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame4} alt="" /></div>
+                            <div>
+                                <strong>Books</strong>
+                                <p>Promote books on your Linktree</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame5} alt="" /></div>
+                            <div>
+                                <strong>Buy Me A Gift</strong>
+                                <p>Let visitors support you with a small gift</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame6} alt="" /></div>
+                            <div>
+                                <strong>Cameo</strong>
+                                <p>Make impossible fan connections possible</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame7} alt="" /></div>
+                            <div>
+                                <strong>Clubhouse</strong>
+                                <p>Let your community in on the conversation</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame8} alt="" /></div>
+                            <div>
+                                <strong>Community</strong>
+                                <p>Build an SMS subscriber list</p>
+                            </div>
+                        </div>
+
+                        <div className={style.appCards}>
+                            <div className={style.icon}><img src={frame1} alt="" /></div>
+                            <div>
+                                <strong>Contact Details</strong>
+                                <p>Easily share downloadable contact details</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* container7 end */}
+
+                {/* footer */}
+                <div className={style.footerContainer}>
+                    <div className={style.inner_footer}>
+                        <div className={style.footerButtons}>
+                            <button className={style.loginBtn}>Log in</button>
+                            <button className={style.signupBtn}>Sign up free</button>
+                        </div>
+
+                        <div className={style.footerLinks}>
+                            <ul>
+                                <li>About Spark</li>
+                                <li>Blog</li>
+                                <li>Press</li>
+                                <li>Social Good</li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                        <div className={style.footerLinks}>
+                        <ul>
+                            <li>Careers</li>
+                            <li>Getting Started</li>
+                            <li>Features and How-Tos</li>
+                            <li>FAQs</li>
+                            <li>Report a Violation</li>
+                        </ul>
+                        </div>
+                        <div className={style.footerLinks}>
+                        <ul>
+                            <li>Terms and Conditions</li>
+                            <li>Privacy Policy</li>
+                            <li>Cookies Notice</li>
+                            <li>Trust Center</li>
+                        </ul>
+                        </div>
+                    </div>
+
+                    <div className={style.acknowledgement}>
+                    <p >
+                        We acknowledge the Traditional Custodians of the land on which our office stands, The Wurundjeri people of the Kulin Nation, and pay our respects to Elders past, present and emerging.
+                    </p>
+
+                    <div className={style.socialIcons}>
+                        <FaTwitter />
+                        <FaInstagram />
+                        <FaYoutube />
+                        <FaTiktok />
+                        <FaFire />
+                    </div>
+                    </div>
+                </div>
+
+
+
 
             </div>
 
